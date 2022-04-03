c1e883dbfefad972ab856c6cfbb208ecf9b6a9b7
Sourabh <55790282+sourabh3497@users.noreply.github.com>
2022-04-03 09:56:07 +0530

    Update geocode.js

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('postman-request');
 
 const geocode = (address, callback) => {
-    const apikey = 'pk.eyJ1Ijoic291cmFiaDExMTEiLCJhIjoiY2tibjk0YTlvMXFmZDJ5bHN6dHBhcGo1eiJ9.EJQquYCy0fSrh2gCP6tlmQ';
+    const apikey = '';
     const limit = 2;
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${apikey}&limit=${limit}`;
 
